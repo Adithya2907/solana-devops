16927227bb8cbcfa9395ff5f43b21748133bdb91
Sidharth Anand <sidharthanand85@gmail.com>
2023-03-11 03:06:51 +0530

    Added build page details

--- src/routes/repo/[name]/build/[id]/+page.server.ts
+++ src/routes/repo/[name]/build/[id]/+page.server.ts
@@ -2,6 +2,10 @@ import type { PageServerLoad } from './$types';
 
 import { error } from '@sveltejs/kit';
 
+import * as AWS from '@aws-sdk/client-s3';
+
+import { AWS_S3_ACCESS_KEY, AWS_S3_SECRET_KEY } from '$env/static/private';
+
 import db from '$lib/db/client';
 
 export const load = (async ({ params, parent }) => {
@@ -17,10 +21,26 @@ export const load = (async ({ params, parent }) => {
         }
     });
 
+    const s3 = new AWS.S3({
+        credentials: {
+            accessKeyId: AWS_S3_ACCESS_KEY,
+            secretAccessKey: AWS_S3_SECRET_KEY
+        },
+        region: 'ap-south-1'
+    });
+
     if (build === null || build?.listener?.repoID !== repo?.id)
         throw error(404, 'Could not find build');
 
+    const result = await s3.getObject({
+        Bucket: 'idl-files',
+        Key: build.log
+    });
+
+    const log = await result.Body?.transformToString();
+
     return {
-        build
+        build,
+        log
     };
 }) satisfies PageServerLoad;
--- src/routes/repo/[name]/build/[id]/+page.svelte
+++ src/routes/repo/[name]/build/[id]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import { ListenerType } from '@prisma/client';
 
 	export let data: PageData;
 </script>
@@ -7,3 +8,31 @@
 <h1>Build</h1>
 <p>Status: {data.build.status}</p>
 <p>Conclusion: {data.build.conclusion}</p>
+<p>Type: {data.build.listener.type}</p>
+<p>Started: {data.build.started}</p>
+<p>Ended: {data.build.ended}</p>
+<p>
+	{#if data.build.listener.type == ListenerType.PULL_REQUEST}
+		<a href="{data.repo.url}/issues/{data.build.issue}">View on github</a>
+	{/if}
+</p>
+<p>
+	{#if data.build.listener.type == ListenerType.PULL_REQUEST}{/if}
+</p>
+<p>Build logs</p>
+<pre>
+	{#each data.log as line}
+		{line}
+	{/each}
+</pre>
+
+<style>
+	pre {
+		background: black;
+		color: white;
+		white-space: pre-wrap;
+		height: 200px;
+		width: 50%;
+		overflow: scroll;
+	}
+</style>
