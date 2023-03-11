cce329db6c14efd2b4688252e362e07e49a9b44a
Sidharth Anand <sidharthanand85@gmail.com>
2023-03-11 19:45:15 +0530

    Removed docker shit

--- src/lib/platform/build.sh
+++ src/lib/platform/build.sh
@@ -1,4 +1,10 @@
-echo "Building ${_BUILD_REPO_PATH} with ${_BUILD_ANCHOR_VERSION}"
-docker build -t automation --build-arg anchor_version="${_BUILD_ANCHOR_VERSION}" $_BUILD_REPO_PATH
-echo "Running"
-docker run -v ${_BUILD_REPO_PATH}/artifacts:/app/target automation
+avm install ${_BUILD_ANCHOR_VERSION}
+avm use ${_BUILD_ANCHOR_VERSION}
+
+cur=$(pwd)
+cd "${_BUILD_REPO_PATH}/repo"
+
+log=$(anchor build 2>&1)
+
+echo "${log}"
+cd "${cur}"
