2b14747c5b56c09e2ebf4a29bec35974509ad364
Sidharth Anand <sidharthanand85@gmail.com>
2023-03-15 03:11:55 +0530

    landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,6 @@
 
 	import { PUBLIC_GITHUB_ACCESS_URL } from '$env/static/public';
 	import { invalidate } from '$app/navigation';
-	import { user } from '$lib/stores/user.store';
 	import Header from './components/header.svelte';
 
 	export let data: PageData;
@@ -28,33 +27,141 @@
 		<p>Authenticating...</p>
 		<form bind:this={tokenform} method="POST" action="?/token" use:enhance />
 	{:else}
-		<form method="POST" action="?/login">
-			<button type="submit">Login with github</button>
-		</form>
+		<section class="top-section">
+			<div class="overlay">
+				<div class="container">
+					<div class="column">
+						<div class="wrap">
+							<section class="inner">
+								<div class="container">
+									<div class="column column-50">
+										<div class="wrap">
+											<div class="text-highlight">DevOps for Solana</div>
+											<div class="text-heading">Manage your Solana app</div>
+											<div class="text-sub">
+												Configure an automated build and delivery system for both your solana backend and frontend
+												with the click of a button.
+											</div>
+											<div style="margin-bottom: 20px">
+												<div style="margin: 10px 0 0 0">
+													<form method="POST" action="?/login">
+														<button type="submit" class="primary-btn">Get Started</button>
+													</form>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="column column-50">
+										<div class="wrap img-wrap">
+											<img
+												alt="product"
+												class="top-img"
+												src="https://apka.b-cdn.net/wp-content/uploads/2022/08/ketjng-636x1024.png"
+											/>
+										</div>
+									</div>
+								</div>
+							</section>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="top-section">
+			<div class="overlay">
+				<div class="container">
+					<div class="column column-100">
+						<div class="wrap">
+							<section class="inner">
+								<div class="container container-small">
+									<div class="column">
+										<div class="wrap" style="align-content: baseline">
+											<div class="text-highlight" style="text-align: center; width: 100%;">
+												features
+											</div>
+											<div class="heading-small">Managing your app has never been easier</div>
+											<div class="text-sub">
+												Through innovative products, tools, and services, we help you do something
+											</div>
+										</div>
+									</div>
+								</div>
+							</section>
+							<section class="inner">
+								<div class="container">
+									<div class="column column-3">
+										<div class="square">
+											<div class="wcontainer">
+												<img
+													alt="icon"
+													class="icon"
+													src="https://apka.b-cdn.net/wp-content/uploads/2022/08/filejh.png"
+												/>
+											</div>
+											<div class="heading-small">Feature 1</div>
+											<div class="text-sub">
+												View enhanced transactions in real time and keep track of your spending.
+											</div>
+										</div>
+									</div>
+									<div class="column column-3">
+										<div class="square primary">
+											<div class="wcontainer">
+												<img
+													alt="icon"
+													class="icon"
+													src="https://apka.b-cdn.net/wp-content/uploads/2022/08/activehb.png"
+												/>
+											</div>
+											<div class="heading-small">Feature 1</div>
+											<div class="text-sub">
+												View enhanced transactions in real time and keep track of your spending.
+											</div>
+										</div>
+									</div>
+									<div class="column column-3">
+										<div class="square">
+											<div class="wcontainer">
+												<img
+													alt="icon"
+													class="icon"
+													src="https://apka.b-cdn.net/wp-content/uploads/2022/08/transaction.png"
+												/>
+											</div>
+											<div class="heading-small">Feature 1</div>
+											<div class="text-sub">
+												View enhanced transactions in real time and keep track of your spending.
+											</div>
+										</div>
+									</div>
+								</div>
+							</section>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
 	{/if}
 {:else}
 	<Header {data} />
 	<div class="content">
 		<div class="grid grid--1-col">
 			<h2>Installed Repos</h2>
-			<div class="line thick"></div>
+			<div class="line thick" />
 			{#if data.user?.repos}
-			<ul>
-				{#each data.user?.repos as repo}
-				<li><a href="/repo/{repo.name}">{repo.name}</a></li>
-				{/each}
-			</ul>
+				<ul>
+					{#each data.user?.repos as repo}
+						<li><a href="/repo/{repo.name}">{repo.name}</a></li>
+					{/each}
+				</ul>
 			{/if}
 		</div>
-			<span>Can't find your repo?</span>
-			<a href={PUBLIC_GITHUB_ACCESS_URL}>
-				Configure access here
-			</a>
+		<span>Can't find your repo?</span>
+		<a href={PUBLIC_GITHUB_ACCESS_URL}> Configure access here </a>
 	</div>
 {/if}
 
 <style>
-	
 	h2 {
 		padding-left: 20px;
 		font-size: 1.5rem;
@@ -63,6 +170,223 @@
 	li {
 		margin-bottom: 20px;
 	}
-	
-	
+
+	.top-section {
+		background-color: #070c11;
+		background-image: url('https://apka.b-cdn.net/wp-content/uploads/2022/08/wdjch.png');
+		background-position: bottom left;
+		background-repeat: no-repeat;
+		background-size: cover;
+		transition: background 0.3s, border 0.3s, border-radius 0.3s, box-shadow 0.3s;
+		padding: 50px 0px 0px 0px;
+		min-height: 100vh;
+	}
+
+	.top-img {
+		max-width: 80%;
+		vertical-align: middle;
+		display: inline-block;
+		height: auto;
+		border: none;
+		border-radius: 0;
+		box-shadow: none;
+	}
+
+	.overlay {
+		background-color: transparent;
+		background-image: linear-gradient(180deg, #070c1100 87%, #070c11 100%);
+		opacity: 1;
+		transition: background 0.3s, border-radius 0.3s, opacity 0.3s;
+	}
+
+	.container {
+		display: flex;
+		margin-right: auto;
+		margin-left: auto;
+		position: relative;
+	}
+
+	.column {
+		position: relative;
+		min-height: 1px;
+		display: flex;
+		width: 100%;
+	}
+
+	.wrap {
+		align-content: center;
+		align-items: center;
+		margin: 0px 0px 0px 0px;
+		--e-column-margin-right: 0px;
+		--e-column-margin-left: 0px;
+		padding: 0px 0px 0px 0px;
+		position: relative;
+		width: 100%;
+		flex-wrap: wrap;
+		display: flex;
+	}
+
+	.wrap.img-wrap {
+		background-image: url('https://apka.b-cdn.net/wp-content/uploads/2022/08/ektmg.png');
+		background-position: center center;
+		background-repeat: no-repeat;
+		background-size: contain;
+		transition: background 0.3s, border 0.3s, border-radius 0.3s, box-shadow 0.3s;
+		margin: 0px 0px 0px 60px;
+		padding: 0px 0px 0px 0px;
+	}
+
+	.wrap:not(:last-child) {
+		margin-bottom: 20px;
+	}
+
+	.inner {
+		width: 100%;
+	}
+
+	.column-50 {
+		width: 50%;
+	}
+
+	.text-highlight {
+		columns: 1;
+		column-gap: 0px;
+		color: #61d08c;
+		font-family: 'DM Sans', Sans-serif;
+		font-size: 16px;
+		font-weight: 700;
+		text-transform: uppercase;
+		line-height: 19px;
+		letter-spacing: 1px;
+		word-spacing: 3px;
+		transition: background 0.3s, border 0.3s, border-radius 0.3s, box-shadow 0.3s,
+			transform var(--e-transform-transition-duration, 0.4s);
+		margin-bottom: 20px;
+	}
+
+	.text-heading {
+		color: #ffffff;
+		font-family: 'Space Mono', Sans-serif;
+		font-size: 80px;
+		font-weight: 600;
+		line-height: 1.1em;
+		letter-spacing: -0.5px;
+		width: auto;
+		transition: background 0.3s, border 0.3s, border-radius 0.3s, box-shadow 0.3s,
+			transform var(--e-transform-transition-duration, 0.4s);
+		margin-bottom: 20px;
+	}
+
+	.primary-btn {
+		font-family: 'Space Mono', Sans-serif;
+		font-size: 18px;
+		font-weight: 700;
+		letter-spacing: -0.5px;
+		fill: #041307;
+		color: #041307;
+		background-color: #63d28e;
+		border-style: solid;
+		border-width: 0px 0px 0px 0px;
+		border-color: #63d28e;
+		border-radius: 8px 8px 8px 8px;
+		box-shadow: 0px 10px 20px 0px rgba(255, 255, 255, 0);
+		padding: 17px 35px 17px 35px;
+		cursor: pointer;
+		transition: all 0.45s;
+	}
+
+	.primary-btn:hover {
+		background-color: #f8d885;
+	}
+
+	.text-sub {
+		text-align: left;
+		color: #b3b3b3;
+		font-family: 'DM Sans', Sans-serif;
+		font-size: 20px;
+		font-weight: 400;
+		line-height: 1.9em;
+		margin: 20px 0px 20px 0px;
+		padding: 0px 0px 0px 0px;
+		transition: background 0.3s, border 0.3s, border-radius 0.3s, box-shadow 0.3s,
+			transform var(--e-transform-transition-duration, 0.4s);
+	}
+
+	.container {
+		max-width: 1200px;
+		min-height: 790px;
+	}
+
+	.container-small {
+		max-width: 600px;
+	}
+
+	.heading-small {
+		color: #ffffff;
+		font-family: 'Space Mono', Sans-serif;
+		font-size: 43px;
+		font-weight: 600;
+		line-height: 1.3em;
+		letter-spacing: 0px;
+	}
+
+	.container-small div {
+		width: 100%;
+		text-align: center;
+	}
+
+	.column-3 {
+		width: 33.333%;
+		margin: 25px;
+	}
+
+	.square {
+		background-color: #22262a;
+		border-radius: 20px 20px 20px 20px;
+		display: flex;
+		position: relative;
+		width: 100%;
+		flex-wrap: wrap;
+		align-content: flex-start;
+		flex-direction: column;
+		align-items: center;
+		padding: 25px;
+	}
+
+	.wcontainer {
+		margin: 0px 0px 20px 0px;
+	}
+
+	.icon {
+		max-width: 80px !important;
+		height: auto;
+		max-width: 100%;
+		border: none;
+		border-radius: 0;
+		box-shadow: none;
+		vertical-align: middle;
+		display: inline-block;
+	}
+
+	.inner .container {
+		min-height: 0;
+		margin-bottom: 50px;
+	}
+
+	.square.primary {
+		background-color: #63D28E;
+		color: #151515;
+	}
+
+	.square .heading-small {
+		font-size: 22px;
+	}
+
+	.square.primary .heading-small {
+		color: #151515;
+	}
+
+	.square.primary .text-sub {
+		color: #151515;
+	}
 </style>
